b39bdf476899a5f91994c94b3a2a162f552b2f5c
Samy Kacimi <samy@cyberwatch.fr>
2018-10-01 14:58:45 +0200

    python 3 migration

--- httpie_cbw_api_auth.py
+++ httpie_cbw_api_auth.py
@@ -9,9 +9,9 @@ import datetime
 from httpie.plugins import AuthPlugin
 
 try:
-    import urlparse
+    from urllib.parse import urlparse
 except ImportError:
-    import urllib.parse
+    from urlparse import urlparse
 
 __version__ = '0.0.2'
 __author__ = 'CyberWatch SAS'
@@ -21,17 +21,21 @@ __licence__ = 'MIT'
 class CbwApiAuth:
     def __init__(self, access_id, secret_key):
         self.access_id = access_id
-        self.secret_key = secret_key.encode('ascii')
+        self.secret_key = secret_key
 
     def __call__(self, r):
         method = r.method.upper()
 
         content_type = r.headers.get('content-type')
-        if not content_type:
+        if content_type:
+            content_type = content_type.decode()
+        else:
             content_type = ''
 
         content_md5 = r.headers.get('content-md5')
-        if not content_md5:
+        if content_md5:
+            content_md5 = content_md5.decode()
+        else:
             content_md5 = ''
 
         httpdate = r.headers.get('date')
@@ -40,7 +44,7 @@ class CbwApiAuth:
             httpdate = now.strftime('%a, %d %b %Y %H:%M:%S GMT')
             r.headers['Date'] = httpdate
 
-        url = urlparse.urlparse(r.url)
+        url = urlparse(r.url)
         path = url.path
         if url.query:
             path = path + '?' + url.query
@@ -51,9 +55,7 @@ class CbwApiAuth:
                               path,
                               httpdate])
 
-        digest = hmac.new(self.secret_key, canonical, hashlib.sha256).digest()
-
-        signature = base64.encodestring(digest).rstrip()
+        signature = (base64.b64encode(hmac.new(self.secret_key.encode('utf-8'), canonical.encode('utf-8'), hashlib.sha256).digest())).decode()
 
         r.headers['Authorization'] = 'CyberWatch APIAuth-HMAC-SHA256 %s:%s' % (
                                       self.access_id,
@@ -68,5 +70,5 @@ class CbwApiAuthPlugin(AuthPlugin):
     auth_type = 'cbw-api-auth'
     description = 'Sign requests using the CyberWatch ApiAuth HMAC-SHA256 authentication method'
 
-    def get_auth(self, access_id, secret_key):
-        return CbwApiAuth(access_id, secret_key)
+    def get_auth(self, username=None, password=None):
+        return CbwApiAuth(username, password)
